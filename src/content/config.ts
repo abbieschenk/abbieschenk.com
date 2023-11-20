849b11c5326d4424d80be2f13876274f1b2510ff
Abbie Schenk <abbieschenk@gmail.com>
2023-11-19 17:02:11 -0700

    added @ alias

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,6 @@
-// 1. Import utilities from `astro:content`
 import { defineCollection } from 'astro:content';
-import { projectFrontmatterSchema } from '../types';
+
+import { projectFrontmatterSchema } from '@/types';
 
 const projectCollection = defineCollection({
     type: 'content',
