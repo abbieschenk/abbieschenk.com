79f889680d18a2698af502c17fdaeacc80322a71
Abbie Schenk <abbieschenk@gmail.com>
2023-12-02 17:33:38 -0700

    first commit with seemingly everything working

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,40 @@
 export default {
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                accent: {
+                    100: '#4BE48D',
+                    200: '#41C67B',
+                    300: '#38AA69',
+                    400: '#2E8B57',
+                    500: '#1C5534',
+                    600: '#123722',
+                    700: '#091B11',
+                },
+            },
+
+            typography: {
+                DEFAULT: {
+                    css: {
+                        a: {
+                            color: '#2E8B57',
+                            'text-decoration-style': 'dotted',
+                            '&hover': {
+                                color: '#1C5534',
+                            },
+                            '&active': {
+                                color: '#41C67B',
+                            },
+                        },
+                    },
+                },
+            },
+        },
+        fontFamily: {
+            sans: 'proxima-nova, Helvetica, Arial, sans-serif',
+            serif: 'serif',
+        },
     },
     plugins: [require('@tailwindcss/typography')],
 };
