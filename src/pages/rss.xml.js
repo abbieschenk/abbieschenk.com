441c625b02b64a243c2489e82ba7153da1145aa6
Abbie Schenk <abbieschenk@gmail.com>
2024-02-10 22:09:03 -0700

    Updated my bio and experience section, and the meta description.

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,7 +3,8 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 export async function GET(context) {
     return rss({
         title: 'Abbie Schenk',
-        description: 'Software Developer + Photographer',
+        description:
+            'Software developer + photographer building technologies and using interdisciplinary methods to convey information in better ways.',
         site: context.site,
         items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
         customData: `<language>en-us</language>`,
