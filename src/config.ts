675416b7169dfa05aa1b713db0220abe76dee2a1
Abbie Schenk <abbieschenk@gmail.com>
2025-02-16 10:06:17 +0100

    fix icons: use spritesheet, add missing attribution, define more dynamically

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,11 @@
-export default {
-    cloudinary: {
-        cloudName: 'abbieschenk',
-    },
-};
+import type { IconLink } from './types';
+
+const follow: IconLink[] = [
+    { href: 'https://github.com/abbieschenk', icon: 'github' },
+    { href: 'https://www.linkedin.com/in/abbie-schenk/', icon: 'linkedin' },
+    { href: 'https://bsky.app/profile/abbieschenk.com', icon: 'bluesky' },
+    { href: 'https://instagram.com/abbieschenk', icon: 'instagram' },
+    { href: 'mailto:abbieschenk@protonmail.ch', icon: 'mail' },
+];
+
+export default { cloudinary: { cloudName: 'abbieschenk' }, links: { follow } };
---
+++ src/types/icons.ts
@@ -0,0 +1,4 @@
+export type FontAwesomeIconVariant = 'bluesky' | 'github' | 'instagram' | 'linkedin' | 'mail';
+export type IconVariant = FontAwesomeIconVariant;
+
+export type IconLink = { href: string; icon: IconVariant };
